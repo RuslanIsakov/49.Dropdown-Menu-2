a6f41330cd6a312d786315bb0350339c1a9ac69d
Ruslan Rustanovich ISAKOV <46198640+RuslanIsakov@users.noreply.github.com>
2020-01-23 07:39:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -66,3 +66,6 @@ if (input.val() == input.attr('placeholder')) {
 		 	var input = $(this);
 		  	if (input.val() == '' || input.val() == input.attr('placeholder')) {
 				input.val(input.attr('placeholder'));
+}
+		}).blur();
+		$('[placeholder]').parents('form').submit(function() {
